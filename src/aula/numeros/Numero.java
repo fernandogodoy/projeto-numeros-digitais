f5e995c52afa9b172628ce1ce73d41a8f74bfae7
Fernando Godóy <fernando.godoi@matera.com>
2015-10-27 11:51:55 -0200

    Ajuste no projeto

---
+++ src/aula/numeros/Numero.java
@@ -0,0 +1,44 @@
+package aula.numeros;
+
+/**
+ * 
+ * Define os métodos necesários para a impressão dos números no formato digital
+ */
+public interface Numero {
+
+	/**
+	 * Texto da primeira linha
+	 * 
+	 * @return Texto a ser impresso
+	 */
+	String getPrimeira();
+
+	/**
+	 * Texto da segunda linha
+	 * 
+	 * @return Texto a ser impresso
+	 */
+	String getSegundaLinha();
+
+	/**
+	 * Texto da terceira linha
+	 * 
+	 * @return Texto a ser impresso
+	 */
+	String getTerceira();
+
+	/**
+	 * Texto da quarta linha
+	 * 
+	 * @return Texto a ser impresso
+	 */
+	String getQuartaLinha();
+
+	/**
+	 * Texto da quinta linha
+	 * 
+	 * @return Texto a ser impresso
+	 */
+	String getQuintaLinha();
+
+}
---
+++ src/aula/util/Constants.java
@@ -0,0 +1,21 @@
+package aula.util;
+
+/**
+ * 
+ * Constants representando Strings comuns utiliza pela aplicação 
+ */
+public abstract class Constants {
+
+	public static final String ZERO = "0";
+	public static final String UM = "1";
+	public static final String DOIS = "2";
+	public static final String TRES = "3";
+	public static final String QUATRO = "4";
+	public static final String CINCO = "5";
+	public static final String SEIS = "6";
+	public static final String SETE = "7";
+	public static final String OITO = "8";
+	public static final String NOVE = "9";
+	public static final String STRING_VAZIA = " ";
+	public static final String QUEBRA_LINHA = "\n";
+}
